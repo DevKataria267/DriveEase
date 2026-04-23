480cc790421ebd7ce603ebfd8a76c0573ef4bf21
DevKataria267 <dev.kataria267@education.nsw.gov.au>
2026-04-23 14:17:52 +1000

    updated

--- db.py
+++ db.py
@@ -8,7 +8,10 @@ def GetDB():
 
 def GetAllSessions():
     db = GetDB()
-    sessions = db.execute("SELECT * FROM Sessions").fetchall()
+    sessions = db.execute("""SELECT Sessions.date, Sessions.duration, Sessions.distance,
+        Sessions.supervisor, Sessions.time_of_day, Users.username
+        FROM Sessions JOIN Users ON Sessions.user_id = Users.id
+        ORDER BY date DESC""").fetchall()
     db.close()
     return sessions
 
@@ -19,3 +22,21 @@ def CheckLogin(username, password):
         if check_password_hash(user['password'], password):
             return user
     return None
+
+def RegisterUser(username, password):
+    if username is None or password is None:
+        return False
+    db = GetDB()
+    hash = generate_password_hash(password)
+    db.execute("INSERT INTO Users(username, password) VALUES(?, ?)", (username, hash,))
+    db.commit()
+    return True
+
+def AddSession(user_id, date, duration, distance, supervisor, time_of_day):
+    if date is None or duration is None:
+        return False
+    db = GetDB()
+    db.execute("INSERT INTO Sessions(user_id, date, duration, distance, supervisor, time_of_day) VALUES (?, ?, ?, ?, ?, ?)",
+    (user_id, date, duration, distance, supervisor, time_of_day,))
+    db.commit()
+    return True
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import db
 
 app = Flask(__name__)
@@ -6,11 +6,50 @@ app.secret_key = "driveease"
 
 @app.route("/")
 def Home():
-    sessionData = db.GetAllSessions()
-    return render_template("index.html", sessions=sessionData)
+    if not session.get('username'):
+        return redirect("/register")
+    sessionsData = db.GetAllSessions()
+    return render_template("index.html", sessions=sessionsData)
 
 @app.route("/login", methods=["GET", "POST"])
-def Login():
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = db.CheckLogin(username, password)
+        if user:
+            session["id"] = user["id"]
+            session["username"] = username
+            return redirect("/")
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if db.RegisterUser(username, password):
+            return redirect("/")
+    return render_template("register.html")
+
+@app.route("/add", methods=["GET", "POST"])
+def Add():
+    if session.get('username') == None:
+        return redirect("/")
+    if request.method == "POST":
+        user_id = session['id']
+        date = request.form['date']
+        duration = request.form['duration']
+        distance = request.form['distance']
+        supervisor = request.form['supervisor']
+        time_of_day = request.form['time_of_day']
+        db.AddSession(user_id, date, duration, distance, supervisor, time_of_day)
+        return redirect("/")
+    return render_template("add.html")
 
 app.run(debug=True, port=5000)
